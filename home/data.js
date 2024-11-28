16eeb5bf495a4a17b7f61bdcb8cfa14020d7c951
jensen0858 <jensen.kung@gmail.com>
2024-11-28 11:56:08 +0800

    Home

---
+++ home/data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-home",
+      "name": "home",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.3907854347022308,
+        "pitch": 0.5167791802184638,
+        "fov": 1.6370630936749038
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Home",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
