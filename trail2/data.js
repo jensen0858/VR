9ca2f33bfd8d71567547fa448ee3f0f66057456c
jensen0858 <jensen.kung@gmail.com>
2024-12-05 11:04:46 +0800

    trail2

---
+++ trail2/data.js
@@ -0,0 +1,278 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "最後一彎",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.26529004630313224,
+        "pitch": 0.028029181020066574,
+        "fov": 1.487415225140046
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.743120586078815,
+          "pitch": -0.09172935724273756,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.4481453074436814,
+          "pitch": 0.026642948965761803,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.014970382481523714,
+          "pitch": -0.2091163205479667,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6617136946783244,
+          "pitch": -0.09327399834191752,
+          "title": "起跑線",
+          "text": "起跑"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "觀眾席",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.7307111801682886,
+        "pitch": -0.2398052153939041,
+        "fov": 1.487415225140046
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.022768340676464,
+          "pitch": -0.0751081106488467,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.6108104023385739,
+          "pitch": -0.37245424182977693,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.034932487649699695,
+          "pitch": -0.11401775020705074,
+          "title": "安全車停放處",
+          "text": "有撞車發生要出動"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "入維修站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.21874793291662797,
+        "pitch": 0.012457413786691518,
+        "fov": 1.487415225140046
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.90745800177001,
+          "pitch": -0.3002299422706578,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 3.082001279821844,
+          "pitch": 0.040092728862056504,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.8578370553888544,
+          "pitch": -0.09622977249602727,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.01723644879704267,
+          "pitch": 0.20482547919682936,
+          "title": "限速",
+          "text": "99km/hr"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "摩天輪",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.378014134687053,
+        "pitch": -0.7003702662480205,
+        "fov": 1.487415225140046
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4362631028885193,
+          "pitch": 0.06863355328517251,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.08458493524676136,
+          "pitch": 0.04381202717189048,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9582040794833722,
+          "pitch": -1.1633120361143412,
+          "title": "最高點",
+          "text": "50公尺"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "兒童卡丁車",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.502529461150118,
+        "pitch": 0.11582351839391336,
+        "fov": 1.487415225140046
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0125674796367505,
+          "pitch": -0.08129939874236314,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.304589714478972,
+          "pitch": 0.004143873610852111,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7743902122478756,
+          "pitch": 0.01629050806696064,
+          "title": "卡丁車競速",
+          "text": "甩尾"
+        }
+      ]
+    }
+  ],
+  "name": "trail2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
